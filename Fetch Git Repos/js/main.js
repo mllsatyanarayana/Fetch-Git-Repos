5d989926ab1d3f3cfcce1f240c0955ac2011b097
Mylavarapu Satyanarayana <mllsatyanarayana@gmail.com>
2021-05-10 21:25:02 -0700

    updated

--- Fetch Git Repos/js/main.js
+++ Fetch Git Repos/js/main.js
@@ -1,6 +1,5 @@
 
 
-
 var form = document.getElementById("myForm")
 
 
@@ -14,16 +13,27 @@ var orginalName = search.split(' ').join('')
 
 //alert(orginalName)
 
-fetch("https://api.github.com/users/"+orginalName)
-.then((result) => result.json())
-.then((data) => {
+//fetch("https://api.github.com/users/"+orginalName)
+fetch('https://api.github.com/users/'+orginalName+'/repos')
+  .then((result) => result.json())
+  .then((data) => {
+  
+    console.log(data);
+    var statusHTML = '';
+      $.each(data, function(i, status) {
+    statusHTML += '<tr>';
+    statusHTML += '<td>' + status.id + '</td>';
+    statusHTML += '<td>' + status.name + '</td>';
+    statusHTML += '<td>' + status.html_url + '</td>';
+    statusHTML += '<td>' + status.language + '</td>';
+    statusHTML += '<td>' + status.full_name + '</td>';
+    statusHTML += '</tr>';
+  });
+  $('tbody').html(statusHTML);
+ 
+    document.getElementById("tbody").innerHTML =  statusHTML; 
+  }
 
-  console.log(data)
-
-  document.getElementById("result").innerHTML = `
-
-     <img src="${data.avatar_url}"/>
-  `
-  })
-
-})
+  )
+}
+)
